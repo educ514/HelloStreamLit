3d9570aea8eefd84ccbeaf6e52c9dae0acb4b492
educ514 <beopyun.kim@gmail.com>
2026-07-06 22:52:15 +0900

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- webMain.py
+++ webMain.py
@@ -4,10 +4,11 @@
 # ══════════════════════════════════════════════════════════════════════════════════════════
 # 외부모듈 영역
 # ══════════════════════════════════════════════════════════════════════════════════════════
-# import sys                                  # system 모듈(Built-In)
-# import os                                   # os 모듈(Built-In)
-# import time                                 # time 모듈(Built-In)
-# import datetime                             # datetime 모듈(Built-in)
+import sys                                  # system 모듈(Built-In)
+import os                                   # os 모듈(Built-In)
+import time                                 # time 모듈(Built-In)
+import datetime                             # datetime 모듈(Built-in)
+import traceback                            # datetime 모듈(Built-in)
 
 import numpy as np                          # Numpy 모듈(pip install numpy)
 # import pandas as pd                         # Pandas 모듈(pip install pandas)
@@ -16,9 +17,6 @@ import streamlit as st                      # streamlit 모듈(pip install strea
 
 # import Css                                  # CSS 모듈(User)
 import webMainMgr                           # webMainMgr 모듈(User)
-
-# from ComMgr import Common as Com            # 공통 모듈(User)
-import ExceptionMgr                         # 예외 처리 모듈(User)
 # ══════════════════════════════════════════════════════════════════════════════════════════
 # 사용자정의 클래스 영역
 # ------------------------------------------------------------------------------------------
@@ -121,7 +119,7 @@ class Form():
             # Parent2.__init__(self)                  # 부모 클래스가 두개 이상인 경우 부모 객체 생성
             # ==============================================================================
         except Exception as Ex:
-            ExceptionMgr.Except.DisplayException(None)      # 예외처리(콘솔)
+            print(traceback.format_exc())                   # 예외 출력하기(예외처리)
     # ======================================================================================
     # 전역함수 관리 - 필수영역(정적함수)
     # ======================================================================================
